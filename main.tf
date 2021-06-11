1634954d89fd19e110fe8a4c5a1ceb98fc122a80
Oleksii Pryshchepa <olexiiprish@gamil.com>
2021-06-11 17:52:25 +0300

    Update ami

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_webserver" {
-  ami                    = "ami-03a71cec707bfc3d7"
+  ami                    = "ami-089539692cca55c6c"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.my_webserver.id]
   user_data              = <<EOF
