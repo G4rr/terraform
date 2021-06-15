2f105c752ff7991ee187d980f6a5ca585e65d52c
Pryshchepa Oleksii <73528295+G4rr@users.noreply.github.com>
2021-06-15 18:41:20 +0300

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -6,11 +6,11 @@ myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
 
 cat <<EOF > /var/www/html/index.html
 <html>
-<body bgcolor="yellow">
-<h2><font color="gold">Build by Power of Terraform <font color="red"> v0.12</font></h2><br><p>
+<body bgcolor="blue">
+<h2><font color="yellow">Build by Power of Terraform <font color="red"> v0.35</font></h2><br><p>
 <font color="green">Server Private IP: <font color="aqua">$myip<br><br>
 <font color="magenta">
-<b>Version 3.0</b>
+<b>Version 4.0</b>
 </body>
 </html>
 EOF
