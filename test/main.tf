4b9eb221e8c62b9a0cec4f516b501a7ea806ea92
Pryshchepa Oleksii <73528295+G4rr@users.noreply.github.com>
2021-06-18 01:38:34 +0300

    Update main.tf

--- test/main.tf
+++ test/main.tf
@@ -13,7 +13,7 @@ data "aws_ami" "latest_linux" {
 }
 
 resource "aws_key_pair" "key_web" {
-  key_name   = "key"
+  key_name   = "key-web"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDXm7T5/+xU02n8OE8duxZMwczWBqfssp6henwMwmk9qETyczajNdJtEeBs9Ld/BjY7XcjDsOjI1mTeu+bSmneBAIvjUseW+bzYwwA7vbGEfmWAPMwwtEHNiNNb5X2nI/GaMylDkSFMfj+tTZLU5MeQbqXmjUGxMDrd2Wz8dYDITvZ5O0/IHkapOmn0k8LjzlLRd6wfFCnieXTDcSTTiM+tTPla3Pp5qEwodwfdhecoq3iggEy9x9b5A3a9YPpxXBR+h32pwTfRHoMfu65sLReieKTY3MYTBle5s1kIqBwHlTR7x3eUvVdXJ24b1NtVANf/rw70IaCO6lQAgW2sv1/ff9MiEMaJl+e2nXLsVusX+7Q3++8Qa8jNM110I4vROASRBhKhZnGlggYeWeEWetxgvMs1ZAqy6HjnRJoRqbuMyUBwQ1r5u87w8+ePXh3/v9loczCYn7LlZwFYwwPUGq9xQ7Sh/gaTbNjf436iuJxUjbrfKqCRelrBaEAUy6ilUXQ6fcgYIB2HOnc+j/xIgje7pAzPOEIIDTW/E0QHyVctj+K9VQZGV5YHs25vfeTYaiQubKoYyITR2upMQf3s7kFAfreSr64JtlokLIWnoFVARyVyE2m0Zx5gSVc/CoJjmgmTf3U6/e8h7quKL14EY4SMAJBEHRln2ra1QgMgPOEbcw== vanno@LTPE"
 }
 
@@ -32,7 +32,7 @@ resource "aws_instance" "test_instnc" {
 }
 
 resource "aws_security_group" "web_sg" {
-  name        = "Web_SG"
+  name_prefix        = "Web_SG"
   description = "Dynamic SecurityGroup for WebServers"
 
   dynamic "ingress" {
@@ -64,8 +64,8 @@ resource "aws_eip" "test_instnc" {
   vpc      = true
 }
 
-resource "local_file" "file1" {
+resource "local_file" "pub_ip_test" {
   content  = aws_eip.test_instnc.public_ip
-  filename = "../file1.txt"
+  filename = "../pub_ip_test.txt"
 }
 
