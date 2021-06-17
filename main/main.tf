64c3bb932dd70da5c79dfb51d811d71e42843e6e
Pryshchepa Oleksii <73528295+G4rr@users.noreply.github.com>
2021-06-18 01:36:57 +0300

    Update main.tf

--- main/main.tf
+++ main/main.tf
@@ -12,17 +12,17 @@ data "aws_ami" "latest_linux" {
   }
 }
 
-resource "aws_key_pair" "key_web" {
-  key_name   = "key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDXm7T5/+xU02n8OE8duxZMwczWBqfssp6henwMwmk9qETyczajNdJtEeBs9Ld/BjY7XcjDsOjI1mTeu+bSmneBAIvjUseW+bzYwwA7vbGEfmWAPMwwtEHNiNNb5X2nI/GaMylDkSFMfj+tTZLU5MeQbqXmjUGxMDrd2Wz8dYDITvZ5O0/IHkapOmn0k8LjzlLRd6wfFCnieXTDcSTTiM+tTPla3Pp5qEwodwfdhecoq3iggEy9x9b5A3a9YPpxXBR+h32pwTfRHoMfu65sLReieKTY3MYTBle5s1kIqBwHlTR7x3eUvVdXJ24b1NtVANf/rw70IaCO6lQAgW2sv1/ff9MiEMaJl+e2nXLsVusX+7Q3++8Qa8jNM110I4vROASRBhKhZnGlggYeWeEWetxgvMs1ZAqy6HjnRJoRqbuMyUBwQ1r5u87w8+ePXh3/v9loczCYn7LlZwFYwwPUGq9xQ7Sh/gaTbNjf436iuJxUjbrfKqCRelrBaEAUy6ilUXQ6fcgYIB2HOnc+j/xIgje7pAzPOEIIDTW/E0QHyVctj+K9VQZGV5YHs25vfeTYaiQubKoYyITR2upMQf3s7kFAfreSr64JtlokLIWnoFVARyVyE2m0Zx5gSVc/CoJjmgmTf3U6/e8h7quKL14EY4SMAJBEHRln2ra1QgMgPOEbcw== vanno@LTPE"
-}
+#resource "aws_key_pair" "key_web" {
+#  key_name   = "key"
+#  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDXm7T5/+xU02n8OE8duxZMwczWBqfssp6henwMwmk9qETyczajNdJtEeBs9Ld/BjY7XcjDsOjI1mTeu+bSmneBAIvjUseW+bzYwwA7vbGEfmWAPMwwtEHNiNNb5X2nI/GaMylDkSFMfj+tTZLU5MeQbqXmjUGxMDrd2Wz8dYDITvZ5O0/IHkapOmn0k8LjzlLRd6wfFCnieXTDcSTTiM+tTPla3Pp5qEwodwfdhecoq3iggEy9x9b5A3a9YPpxXBR+h32pwTfRHoMfu65sLReieKTY3MYTBle5s1kIqBwHlTR7x3eUvVdXJ24b1NtVANf/rw70IaCO6lQAgW2sv1/ff9MiEMaJl+e2nXLsVusX+7Q3++8Qa8jNM110I4vROASRBhKhZnGlggYeWeEWetxgvMs1ZAqy6HjnRJoRqbuMyUBwQ1r5u87w8+ePXh3/v9loczCYn7LlZwFYwwPUGq9xQ7Sh/gaTbNjf436iuJxUjbrfKqCRelrBaEAUy6ilUXQ6fcgYIB2HOnc+j/xIgje7pAzPOEIIDTW/E0QHyVctj+K9VQZGV5YHs25vfeTYaiQubKoYyITR2upMQf3s7kFAfreSr64JtlokLIWnoFVARyVyE2m0Zx5gSVc/CoJjmgmTf3U6/e8h7quKL14EY4SMAJBEHRln2ra1QgMgPOEbcw== vanno@LTPE"
+#}
 
 resource "aws_security_group" "web_sg" {
-  name        = "Web_SG"
+  name_prefix        = "Web_SG-"
   description = "Dynamic SecurityGroup for WebServers"
 
   dynamic "ingress" {
-    for_each = ["80", "22", "8080"]
+    for_each = ["80", "8080"]
     content {
       from_port   = ingress.value
       to_port     = ingress.value
@@ -117,7 +117,8 @@ resource "aws_default_subnet" "default_az2" {
   availability_zone = data.aws_availability_zones.available.names[1]
 }
 
-resource "local_file" "file2" {
+resource "local_file" "dns" {
   content  = aws_elb.prod_instnc.dns_name
-  filename = "../file2.txt"
+  filename = "../dns.txt"
 }
+
